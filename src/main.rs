a46767ec0d74500f036fb0a2df7b8d1a0c4cd431
vishnu <visank@microsoft.com>
2026-04-17 11:57:54 -0500

    more idiomatic way

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
 use std::env;
 use std::process;
 
-
-fn is_value_hex(value: u32) -> bool {
-    (value >= 97 && value<=102) || (value >=65 && value <=70) || (value >=48 && value <=57)
-}
-
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        eprintln!("Error: No argument provided");
-        process::exit(1);
-    }
-
-    if args.len() > 2 {
-        eprintln!("Error: Too many arguments");
+    if args.len() != 2 {
+        eprintln!("Usage: cargo run -- <string>");
         process::exit(1);
     }
 
     let input: &String = &args[1];
 
-    let is_hex: bool = match input.parse::<u32>() {
-        Ok(value) => is_value_hex(value),
-        Err(_)=>{
-            if let Some(first_char) = input.chars().next() {
-                is_value_hex(first_char as u32)
-            } else {
-                false
-            }
-        }
-    };
+    let is_hex: bool = input.chars().all(|c| c.is_ascii_hexdigit());
 
     if is_hex {
         println!("given argument is a hexadecimal");
